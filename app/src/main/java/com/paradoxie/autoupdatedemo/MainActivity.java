0d3212d7cdd7ae8ba3203a21c1555c0b145339f5
Paradox <xzq351107836@163.com>
2016-10-13 13:53:25 +0800

    update -完成手动配置更新信息

--- app/src/main/java/com/paradoxie/autoupdatedemo/MainActivity.java
+++ app/src/main/java/com/paradoxie/autoupdatedemo/MainActivity.java
@@ -1,14 +1,20 @@
 package com.paradoxie.autoupdatedemo;
 
+import android.Manifest;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.tbruyelle.rxpermissions.RxPermissions;
+
+import rx.functions.Action1;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button check, bugly;
+    UpdateManager manager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,13 +24,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         check.setOnClickListener(this);
         bugly = (Button) findViewById(R.id.bugly_updata);
         bugly.setOnClickListener(this);
+
+        manager = new UpdateManager(MainActivity.this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.check_update:
-                Toast.makeText(this, "heihei", Toast.LENGTH_LONG).show();
+                RxPermissions.getInstance(this)
+                        .request(
+                                //Manifest.permission.READ_PHONE_STATE,
+                                Manifest.permission.WRITE_EXTERNAL_STORAGE)//申请存储权限
+                        .subscribe(new Action1<Boolean>() {
+                            @Override
+                            public void call(Boolean granted) {
+                                if (granted) {
+                                    //同意
+                                    manager.checkUpdate();//点击检查更新；可放到oncreate方法中，则为  自动检测更新
+                                } else {
+                                    //不同意，给提示
+                                    Toast.makeText(MainActivity.this, "检查更新后会将软件包下载至手机，请允许此项权限", Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
                 break;
             case R.id.bugly_updata:
                 startActivity(new Intent(this, BuglyUpdateActivity.class));
---
+++ app/src/main/java/com/paradoxie/autoupdatedemo/UpdateManager.java
@@ -0,0 +1,238 @@
+package com.paradoxie.autoupdatedemo;/**
+ * Created by xiehehe on 16/8/22.
+ */
+
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * User: xiehehe
+ * Date: 2016-08-22
+ * Time: 20:33
+ * FIXME
+ */
+public class UpdateManager {
+    private Context mContext;
+    private static final int DOWNLOAD = 0;//下载
+    private static final int DOWNLOAD_FINISH = 1;//下载完成
+    private String mSavePath;//保存路径
+    private int progress;//进度值
+    private ProgressBar mProgress;//进度条
+    private Dialog mDownloadDialog;//更新窗口
+    private boolean cancelUpdate = false;//取消更新
+    private Handler mHandler = new Handler() {
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                // 正在下载
+                case DOWNLOAD:
+                    // 设置进度条位置
+                    mProgress.setProgress(progress);
+                    break;
+                case DOWNLOAD_FINISH:
+                    // 安装文件
+                    installApk();
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+
+    public UpdateManager(Context context) {
+        this.mContext = context;
+    }
+
+    /**
+     * 检测软件更新
+     */
+    public void checkUpdate() {
+        if (isUpdate()) {
+            // 显示提示对话框
+            showNoticeDialog();
+        } else {
+            Toast.makeText(mContext, "没有新版", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    /**
+     * 判断是否有更新
+     * @return
+     */
+    private boolean isUpdate() {
+        // 获取当前软件版本
+        int versionCode = getVersionCode(mContext);
+
+        // 调用方法获取服务器可用版本信息，此处模拟为大于当前版本的定值
+        int serviceCode = 4;
+        // 版本判断
+        if (serviceCode > versionCode) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 获取本地软件版本号
+     *
+     * @param context
+     * @return
+     */
+    private int getVersionCode(Context context) {
+        int versionCode = 0;
+        try {
+            // 获取软件版本号，对应AndroidManifest.xml下android:versionCode
+            versionCode = context.getPackageManager().getPackageInfo(context.getPackageName(), 0).versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return versionCode;
+    }
+
+    /**
+     * 显示软件更新对话框
+     */
+    private void showNoticeDialog() {
+        // 构造对话框
+        AlertDialog.Builder builder = new Builder(mContext);
+        builder.setTitle(R.string.soft_update_title);
+        builder.setMessage(R.string.soft_update_info);
+        // 更新
+        builder.setPositiveButton(R.string.soft_update_updatebtn, new OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                // 显示下载对话框
+                showDownloadDialog();
+            }
+        });
+        Dialog noticeDialog = builder.create();
+        noticeDialog.show();
+    }
+
+    /**
+     * 显示软件下载对话框
+     */
+    private void showDownloadDialog() {
+        // 构造软件下载对话框
+        AlertDialog.Builder builder = new Builder(mContext);
+        builder.setTitle(R.string.soft_updating);
+        // 给下载对话框增加进度条
+        final LayoutInflater inflater = LayoutInflater.from(mContext);
+        View v = inflater.inflate(R.layout.softupdate_progress, null);
+        mProgress = (ProgressBar) v.findViewById(R.id.update_progress);
+        builder.setView(v);
+        // 取消更新
+        builder.setNegativeButton(R.string.soft_update_cancel, new OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                // 设置取消状态
+                cancelUpdate = true;
+            }
+        });
+        mDownloadDialog = builder.create();
+        mDownloadDialog.show();
+        // 现在文件
+        downloadApk();
+    }
+
+    /**
+     * 下载apk文件
+     */
+    private void downloadApk() {
+        // 启动新线程下载软件
+        new downloadApkThread().start();
+    }
+
+    private class downloadApkThread extends Thread {
+        @Override
+        public void run() {
+            try {
+                // 判断SD卡是否存在，并且是否具有读写权限
+                if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+                    // 获得存储卡的路径
+                    String sdpath = Environment.getExternalStorageDirectory() + "/";
+                    mSavePath = sdpath + "download";
+                    URL url = new URL("http://gdown.baidu.com/data/wisegame/f98d235e39e29031/baiduxinwen.apk");
+                    // 创建连接
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.connect();
+                    // 获取文件大小
+                    int length = conn.getContentLength();
+                    // 创建输入流
+                    InputStream is = conn.getInputStream();
+
+                    File file = new File(mSavePath);
+                    // 判断文件目录是否存在
+                    if (!file.exists()) {
+                        file.mkdir();
+                    }
+                    File apkFile = new File(mSavePath, "软件名字");
+                    FileOutputStream fos = new FileOutputStream(apkFile);
+                    int count = 0;
+                    // 缓存
+                    byte buf[] = new byte[1024];
+                    // 写入到文件中
+                    do {
+                        int numread = is.read(buf);
+                        count += numread;
+                        // 计算进度条位置
+                        progress = (int) (((float) count / length) * 100);
+                        // 更新进度
+                        mHandler.sendEmptyMessage(DOWNLOAD);
+                        if (numread <= 0) {
+                            // 下载完成
+                            mHandler.sendEmptyMessage(DOWNLOAD_FINISH);
+                            break;
+                        }
+                        // 写入文件
+                        fos.write(buf, 0, numread);
+                    } while (!cancelUpdate);// 点击取消就停止下载.
+                    fos.close();
+                    is.close();
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            // 取消下载对话框显示
+            mDownloadDialog.dismiss();
+        }
+    }
+
+    private void installApk() {
+        File apkfile = new File(mSavePath, "软件名字");
+        if (!apkfile.exists()) {
+            return;
+        }
+        //校验MD5
+
+        // 通过Intent安装APK文件
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setDataAndType(Uri.parse("file://" + apkfile.toString()), "application/vnd.android.package-archive");
+        mContext.startActivity(i);
+    }
+}
